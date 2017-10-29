44ed06653b6fa380d1e81b18f1ebd91de3689325
cisterni <cisterni@di.unipi.it>
2017-10-29 09:11:52 +0100

    Lezione del 27/10

--- lwc.fsx
+++ lwc.fsx
@@ -23,29 +23,144 @@ type W2V() =
 let Rect2RectF (r:Rectangle) =
   RectangleF(single r.X, single r.Y, single r.Width, single r.Height)
 
+let RectF2Rect (r:RectangleF) =
+  Rectangle(int r.X, int r.Y, int r.Width, int r.Height)
+
+type CoordinateType = View | World
+
 type LWControl() =
-  let position = PointF()
-  let size = SizeF()
+  let mutable coordinates = View
+
+  let mutable position = PointF()
+  let mutable size = SizeF()
 
   let mutable parent : Control = null
 
+  let mousedownevt = new Event<MouseEventArgs>()
+  let mousemoveevt = new Event<MouseEventArgs>()
+  let mouseupevt = new Event<MouseEventArgs>()
+
+  member this.CoordinateType
+    with get() = coordinates
+    and set(v) = coordinates <- v
+ 
+  member this.Position
+    with get() = position
+    and set(v) = position <- v
+
+  member this.Size
+    with get() = size
+    and set(v) = size <- v
+  
   member this.Parent 
     with get() = parent
     and set(v) = parent <- v
 
-  abstract OnPaint : PaintEventArgs -> unit
-  with default this.OnPaint e = ()
+  member this.MouseDown = mousedownevt.Publish
 
-type LWContainer() =
+  abstract OnMouseDown : MouseEventArgs -> unit
+  default this.OnMouseDown e = mousedownevt.Trigger(e)
+
+  abstract OnMouseMove : MouseEventArgs -> unit
+  default this.OnMouseMove e = mousemoveevt.Trigger(e)
+
+  abstract OnMouseUp : MouseEventArgs -> unit
+  default this.OnMouseUp e = mouseupevt.Trigger(e)
+
+  abstract OnPaint : PaintEventArgs -> unit
+  default this.OnPaint e = ()
+
+
+  abstract HitTest : PointF -> bool
+  default this.HitTest p =
+    (new RectangleF(position, size)).Contains(p)
+
+type LWContainer() as this =
   inherit UserControl()
+
+  let transformPoint (m:Drawing2D.Matrix) (p:PointF) =
+    let pts = [| p |]
+    m.TransformPoints(pts)
+    pts.[0]
+
+  let transform = W2V()
+
+  let controls = ResizeArray<LWControl>()
+
+  let scrollUp () =
+    transform.Translate(0.f, 10.f)
+    this.Invalidate()
+
   
-  let controlsView = ResizeArray<LWControl>()
-  
-  member this.LWControls with get() = controlsView
+  member this.LWControls with get() = controls
+
+  override this.OnMouseDown e =
+    let p = PointF(single e.X, single e.Y)
+    let controlsView = controls |> Seq.filter (fun c -> c.CoordinateType = View)
+    match (controlsView |> Seq.tryFind (fun c -> c.HitTest p)) with
+    | Some c -> c.OnMouseDown(e)
+    | None -> 
+      let pw = transformPoint transform.V2W p
+      let controlsWorld = controls |> Seq.filter (fun c -> c.CoordinateType = World)
+      match (controlsWorld |> Seq.tryFind(fun c -> c.HitTest pw)) with
+      | Some c -> c.OnMouseDown(e)
+      | None -> ()
+
+  override this.OnMouseMove e =
+    let p = PointF(single e.X, single e.Y)
+    let controlsView = controls |> Seq.filter (fun c -> c.CoordinateType = View)
+    match (controlsView |> Seq.tryFind (fun c -> c.HitTest p)) with
+    | Some c -> c.OnMouseMove(e)
+    | None -> ()
+
+  override this.OnMouseUp e =
+    let p = PointF(single e.X, single e.Y)
+    let controlsView = controls |> Seq.filter (fun c -> c.CoordinateType = View)
+    match (controlsView |> Seq.tryFind (fun c -> c.HitTest p)) with
+    | Some c -> c.OnMouseUp(e)
+    | None -> ()
 
   override this.OnPaint e =
     let g = e.Graphics
 
-    controlsView |> Seq.iter(fun c ->
-      c.OnPaint e
-    )
+    let t = g.Transform
+
+    g.Transform <- transform.W2V
+
+    for idx in (controls.Count - 1) .. -1 .. 0 do
+      let c = controls.[idx]
+      if c.CoordinateType = World then
+        c.OnPaint e
+    
+    g.Transform <- t
+
+    for idx in (controls.Count - 1) .. -1 .. 0 do
+      let c = controls.[idx]
+      if c.CoordinateType = View then
+        c.OnPaint e
+
+  override this.OnKeyDown e =
+    match e.KeyCode with
+    | Keys.W -> scrollUp()
+    | Keys.A -> 
+      transform.Translate(10.f, 0.f)
+      this.Invalidate()
+    | Keys.S -> 
+      transform.Translate(0.f, -10.f)
+      this.Invalidate()
+    | Keys.D ->
+      transform.Translate(-10.f, 0.f)
+      this.Invalidate()
+    | Keys.Q ->
+      transform.Rotate(10.f)
+      this.Invalidate()
+    | Keys.E ->
+      transform.Rotate(-10.f)
+      this.Invalidate()
+    | Keys.Z ->
+      transform.Scale(1.1f, 1.1f)
+      this.Invalidate()
+    | Keys.X ->
+      transform.Scale(1.f/1.1f, 1.f/1.1f)
+      this.Invalidate()
+    | _ -> ()
--- lwctest.fsx
+++ lwctest.fsx
@@ -5,22 +5,60 @@ open System.Drawing
 
 open Lwc
 
-type UpButton() =
+type PIButton() =
   inherit LWControl()
+
+  let mutable text = ""
+
+  member this.Text
+    with get() = text
+    and set(v) = text <- v
 
   override this.OnPaint e =
     let parent = this.Parent
     let g = e.Graphics
-    let r = Rectangle(20, 0, 30, 30)
+    let r = RectangleF(this.Position, this.Size) |> RectF2Rect
     g.DrawRectangle(Pens.Red, r)
-    let sz = g.MeasureString("W", parent.Font)
-    g.DrawString("W", parent.Font, Brushes.Red, PointF(20.f + (30.f - sz.Width) / 2.f, (30.f - sz.Height) / 2.f))
+    let ssz = g.MeasureString(text, parent.Font)
+    let p = this.Position
+    let sz = this.Size
+    let sx, sy = p.X + (sz.Width - ssz.Width) / 2.f, p.Y + (sz.Height - ssz.Height) / 2.f
+    g.DrawString(text, parent.Font, Brushes.Red, PointF(sx, sy))
 
-let up = new UpButton()
+type PiHandle() =
+  inherit LWControl()
+
+  override this.OnPaint e =
+    let g = e.Graphics
+
+    let r = RectangleF(this.Position, this.Size) |> RectF2Rect
+    g.DrawEllipse(Pens.Red, r)
+  
+  override this.HitTest p =
+    let pc = this.Position
+    let sz = this.Size
+    let r = sz.Width / 2.f
+    let sqr x = x * x
+
+    let cx, cy = pc.X + r, pc.Y + r
+    sqr(p.X - cx) + sqr(p.Y - cy) <= sqr(r)
+
+let up = PIButton(Position=PointF(20.f, 0.f),Size=SizeF(30.f, 30.f),Text="W")
+let left = PIButton(Position=PointF(0.f, 30.f),Size=SizeF(30.f, 30.f),Text="A",CoordinateType=View)
+
+let point = PiHandle(Position=PointF(50.f, 50.f),Size=SizeF(20.f, 20.f), CoordinateType=World)
+
+up.MouseDown.Add(fun _ -> printfn "W clicked")
+left.MouseDown.Add(fun _ -> printfn "A clicked")
+point.MouseDown.Add(fun _ -> printfn "point clicked")
 
 let c = new LWContainer(Dock=DockStyle.Fill)
 up.Parent <- c
+left.Parent <- c
+point.Parent <- c
 c.LWControls.Add(up)
+c.LWControls.Add(left)
+c.LWControls.Add(point)
 let f = new Form()
 f.Controls.Add(c)
 f.Show()
